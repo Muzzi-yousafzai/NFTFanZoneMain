e1c8905b3e7d749f10fab78d8cc40e55cca8174c
Muzzi-yousafzai <71315090+Muzzi-yousafzai@users.noreply.github.com>
2021-09-29 19:19:00 +0500

    selection
    selection done

--- Assets/_Scripts/ObjectSelection.cs
+++ Assets/_Scripts/ObjectSelection.cs
@@ -6,25 +6,39 @@ using UnityEngine.UI;
 
 public class ObjectSelection : MonoBehaviour
 {
-    
+    public GameObject Ring;
+    public GameObject[] Prefabs;
     private void Awake()
     {
+        
         gameObject.GetComponent<LeanTouch>().enabled = false;
         gameObject.GetComponent<LeanPinchScale>().enabled = false;
         gameObject.GetComponent<LeanTwistRotate>().enabled = false;
+        gameObject.GetComponent<LeanDragTranslate>().enabled = false;
     }
     private void OnMouseUp()
     {
         gameObject.GetComponent<LeanTouch>().enabled = false;
         gameObject.GetComponent<LeanPinchScale>().enabled = false;
         gameObject.GetComponent<LeanTwistRotate>().enabled = false;
+        gameObject.GetComponent<LeanDragTranslate>().enabled = false;
+        
+
     }
     private void OnMouseDown()
     { 
        gameObject.GetComponent<LeanTouch>().enabled = true;
        gameObject.GetComponent<LeanPinchScale>().enabled = true;
-       gameObject.GetComponent<LeanTwistRotate>().enabled = true;  
+       gameObject.GetComponent<LeanTwistRotate>().enabled = true;
+       gameObject.GetComponent<LeanDragTranslate>().enabled = true;
+        GameObject[] objs;
+        objs = GameObject.FindGameObjectsWithTag("Target");
+        foreach (GameObject model in objs)
+        {
+            model.transform.GetChild(0).gameObject.SetActive(false);
+        }
+        Ring.SetActive(true);
     }
-   
+
 }
 
